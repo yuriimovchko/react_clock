2f419f5c538f216fa42c9c2bdf4beae7f305a161
yuriimovchko <movchko.yuriy@gmail.com>
2020-09-04 08:17:06 +0300

    Updates

---
+++ precache-manifest.dd20798419026516f9505e2a3048f0f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3bdf6158e4dfe0eec11775a077af6a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b43fac27ec35e271666",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "8daccb04a7f88300915b",
+    "url": "./static/js/2.754e2893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.754e2893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b43fac27ec35e271666",
+    "url": "./static/js/main.aa0e8021.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
